691b3608644bb765926d4dbe826791f2e3208949
ElWazy <ElCazero.Fer@gmail.com>
2022-10-04 07:52:25 -0300

    Starting Socket io server
    with production and develop modes

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,15 +9,25 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 const server = http.createServer(app)
-const io = new SocketServer(server)
+const io = new SocketServer(server, {
+  cors: {
+    origin: ['http://localhost:5173']
+  }
+})
 
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
-app.use(express.static(join(__dirname, '../../client/dist')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '../../client/dist')))
+}
 
 io.on('connection', socket => {
   console.log(socket.id)
+  socket.on('update_location', location => {
+    console.log(location)
+  })
 })
 
 server.listen(PORT)
