c3f7e8140cf5a9d8f4bb969f527f44b7f43e467a
ElWazy <ElCazero.Fer@gmail.com>
2022-12-05 21:20:18 -0300

    Add database connection (mongodb driver)

--- server/src/services/database.service.ts
+++ server/src/services/database.service.ts
@@ -1,8 +1,14 @@
 import { Collection, MongoClient } from 'mongodb'
 import * as dotenv from 'dotenv'
 import Company from '../models/Company'
+import User from '../models/User'
+import { Location } from '../models/Location'
 
-export const collections: { companies?: Collection<Company> } = {}
+export const collections: {
+  companies?: Collection<Company>,
+  users?: Collection<User>,
+  locations?: Collection<Location>
+} = {}
 
 export async function connectToDatabase() {
   dotenv.config()
@@ -17,9 +23,16 @@ export async function connectToDatabase() {
   const companiesCollection = database.collection<Company>('companies')
   collections.companies = companiesCollection
 
+  const usersCollection = database.collection<User>('users')
+  collections.users = usersCollection
+
+  const locationsCollection = database.collection<Location>('locations')
+  collections.locations = locationsCollection
+
   console.log(
-    `Successfully connected to database ${database.databaseName} and 
-    collection ${companiesCollection.collectionName}`
+    `
+    Successfully connected to database ${database.databaseName} and collection ${companiesCollection.collectionName}
+    `
   )
 }
 
