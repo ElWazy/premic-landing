503510996cfe8f19ee92857b7f81f4cfabff02c2
ElWazy <ElCazero.Fer@gmail.com>
2022-12-05 21:26:22 -0300

    Add endpoints routes to http server

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,10 @@ import morgan from 'morgan'
 import helmet from 'helmet'
 import http from 'http'
 import { join } from 'path'
+
 import { companyRouter } from './routes/CompanyRouter'
+import { userRouter } from './routes/UserRouter'
+import { locationRouter } from './routes/LocationRouter'
 
 const app = express()
 const server = http.createServer(app)
@@ -13,7 +16,9 @@ app.use(morgan('dev'))
 app.use(helmet())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/company', companyRouter)
+app.use('/api/companies', companyRouter)
+app.use('/api/locations', locationRouter)
+app.use('/api/users', userRouter)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(join(__dirname, '../../client/dist')))
